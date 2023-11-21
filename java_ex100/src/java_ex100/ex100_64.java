c3c02ab4a68342f85771ce1a0e5fd5b24943377c
Taehyung Kim <thkim0517@gmail.com>
2023-11-21 15:26:55 +0900

    practice

---
+++ java_ex100/src/java_ex100/ex100_64.java
@@ -0,0 +1,15 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex100_64 {
+
+	 public static void main(String[] args){
+	        Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	    	int b = scan.nextInt();
+	        int c = a%b;
+	        System.out.println(c); 
+	        scan.close();
+	    }
+	}
